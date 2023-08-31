5aee1c2031c4d23cabf06add99bdd8a86d3c9f84
Matt Burton <mattjburton64@gmail.com>
2023-08-31 15:58:58 -0400

    documentation

--- csc4510/assignment1/Script/execute.sh
+++ csc4510/assignment1/Script/execute.sh
@@ -4,24 +4,24 @@
 testFiles=(`ls ./TestFiles`) # Gets a list of all the test files from the ./TestFiles directory
 size=${#testFiles[@]} # Gets the number of files
 
-# Creates directories for the test outputs to be stored
+# Creates directories for the output files
 mkdir -p Output/cpp/correct
 mkdir -p Output/cpp/error
 mkdir -p Output/ada/correct
 mkdir -p Output/ada/error
 
-index=0
-while [ $index -lt $size ] # For every test file...
+fileNum=0
+while [ $fileNum -lt $size ] # For every test file...
 do
-    > "output$((index + 1)).txt" # Creates a new output file for the cpp correct tests
+    > "output$((fileNum + 1)).txt" # Creates a new output file name
     while IFS= read -r line; # For every line in the test file...
     do 
-        # Runs the four programs and store thier results
-        echo "$line" | ./Source/cpp/sumCorrect.o >> Output/cpp/correct/output$((index + 1)).txt 
-        echo "$line" | ./Source/cpp/sumError.o >> Output/cpp/error/output$((index + 1)).txt
-        echo "$line" | ./Source/ada/sumcorrect >> Output/ada/correct/output$((index + 1)).txt 
-        echo "$line" | ./Source/ada/sumerror >> Output/ada/error/output$((index + 1)).txt 
+        # Pipes the line from the test file into each of the four programs and stores thier results
+        echo "$line" | ./Source/cpp/sumCorrect.o >> Output/cpp/correct/output$((fileNum + 1)).txt 
+        echo "$line" | ./Source/cpp/sumError.o >> Output/cpp/error/output$((fileNum + 1)).txt
+        echo "$line" | ./Source/ada/sumcorrect >> Output/ada/correct/output$((fileNum + 1)).txt 
+        echo "$line" | ./Source/ada/sumerror >> Output/ada/error/output$((fileNum + 1)).txt 
 
-    done < "./TestFiles/${testFiles[$index]}"
-    index=$((index + 1))
+    done < "./TestFiles/${testFiles[$fileNum]}" # Passes in each of the test files as input
+    fileNum=$((fileNum + 1)) # Incrementor
 done
