0686c0fe2191c367b4a2906870eab730165a311c
Matt Burton <mattjburton64@gmail.com>
2023-08-31 15:52:59 -0400

    all four programs output stored

--- csc4510/assignment1/Script/execute.sh
+++ csc4510/assignment1/Script/execute.sh
@@ -3,8 +3,12 @@
 
 testFiles=(`ls ./TestFiles`) # Gets a list of all the test files from the ./TestFiles directory
 size=${#testFiles[@]} # Gets the number of files
+
 # Creates directories for the test outputs to be stored
 mkdir -p Output/cpp/correct
+mkdir -p Output/cpp/error
+mkdir -p Output/ada/correct
+mkdir -p Output/ada/error
 
 index=0
 while [ $index -lt $size ] # For every test file...
@@ -12,7 +16,12 @@ do
     > "output$((index + 1)).txt" # Creates a new output file for the cpp correct tests
     while IFS= read -r line; # For every line in the test file...
     do 
-        echo "$line" | ./Source/cpp/sumCorrect.o >> Output/cpp/correct/output$((index + 1)).txt # Run the correct C++ program and store its results
+        # Runs the four programs and store thier results
+        echo "$line" | ./Source/cpp/sumCorrect.o >> Output/cpp/correct/output$((index + 1)).txt 
+        echo "$line" | ./Source/cpp/sumError.o >> Output/cpp/error/output$((index + 1)).txt
+        echo "$line" | ./Source/ada/sumcorrect >> Output/ada/correct/output$((index + 1)).txt 
+        echo "$line" | ./Source/ada/sumerror >> Output/ada/error/output$((index + 1)).txt 
+
     done < "./TestFiles/${testFiles[$index]}"
     index=$((index + 1))
 done
