8631db2fe6a1b852682d24b8313761690089af06
Matt Burton <mattjburton64@gmail.com>
2024-03-26 11:08:58 -0400

    documentation

--- csc4210/assignments/scheduler/schedule_fcfs.c
+++ csc4210/assignments/scheduler/schedule_fcfs.c
@@ -1,3 +1,11 @@
+/*
+Names: Matt Burton, Emma Jerrier, William Phipps
+Date: Mar 26 2024
+Class: CCS4210
+About: This program is designed to implement the first come first serve algorithm when executing a scheduled queue of task
+Compile: make fcfs
+Execute: ./fcfs schedule.txt
+*/
 #include "queue.h"
 #include "task.h"
 #include "cpu.h"
@@ -6,7 +14,12 @@
 #include <string.h>
 #include <stdio.h>
 
-// add a task to the list
+/*
+   Precondition:  A queue is passed in that is not already full
+                  Details about the new task are are also passed in
+   Postcondition: Memory is allocated for the new task
+                  The new task is entered into the queue
+*/
 void add(Queue *queue, char *name, int priority, int burst)
 {
    // Create the new task id
@@ -32,7 +45,8 @@ void add(Queue *queue, char *name, int priority, int burst)
 }
 
 /*
-
+   Precondition: This funciton is called with a queue passed in containing zero or more tasks
+   Postcondition: The first task in the queue is returned and removed from the queue
 */
 Task *pickNextTask(Queue *queue)
 {
@@ -44,6 +58,7 @@ Task *pickNextTask(Queue *queue)
    Precondition: The schedule is called with a queue object containing a list of tasks
    Postcondition: Each task in the queue is executed in the order defined by pickNextTask().
                   After a task is executed it is removed from the queue and its memory is freed up
+                  The queue is emptied
 */
 void schedule(Queue *queue)
 {
